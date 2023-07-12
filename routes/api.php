f237ac0c4c2e30e51861ee51c7c89101943e761c
hugomatheuss <hugo_matheuss@outlook.com>
2023-07-12 00:40:11 -0300

    Correções de CORS.

--- routes/api.php
+++ routes/api.php
@@ -17,7 +17,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::group([
-    'middleware' => 'api',
+    //'middleware' => 'api',
     'prefix' => 'auth'
 ], function ($router) {
     Route::post('/login', [AuthController::class, 'login']);
